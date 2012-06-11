509053715c00a2f7367236841aac9d98d0ffa36a
Alireza Sadeghipour <alireza.sadeghipour.1988@gmail.com>
2012-06-11 03:33:49 +0000

    just two script
    git-svn-id: svn://localhost@190 f1514e4e-b68a-44c6-9dea-45284c435795

---
+++ trunk/doc/Technical/Design/ERD/script/18-alter_table_report_category_add_parent.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `eurb`.`report_category` ADD COLUMN `parent_category_id` BIGINT(20) UNSIGNED NULL  AFTER `description` , 
+  ADD CONSTRAINT `fk_report_category_parent`
+  FOREIGN KEY (`parent_category_id` )
+  REFERENCES `eurb`.`report_category` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_report_category_parent` (`parent_category_id` ASC) ;
---
+++ trunk/doc/Technical/Design/ERD/script/19-alter_table_object_config_change_column_key.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `eurb`.`object_config` CHANGE COLUMN `key` `config_key` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_persian_ci' NOT NULL  ;
